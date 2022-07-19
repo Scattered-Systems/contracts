62acb26cb227f17070c00835872417978618d34d
FL03 <jo3mccain@icloud.com>
2022-07-19 12:04:42 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

---
+++ src/contracts/core/defi/amm.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+/**
+* @title Automated Market Maker (amm)
+* @dev
+*/
+contract AutomatedMarketMaker {
+    constructor() {
+
+    }
+}
