16a03cbd26daf824ff30fa3cd7c1e4b178418b59
FL03 <jo3mccain@icloud.com>
2022-07-30 20:26:02 +0000

    Update

--- src/contracts/components/tokens/ERC721/ERC721.sol
+++ src/contracts/components/tokens/ERC721/ERC721.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
+
+import "./IERC721.sol";
+
+/**
+* @title ERC20
+* @dev https://eips.ethereum.org/eips/eip-721
+*/
+contract ERC721 is IERC721 {
+
+}
